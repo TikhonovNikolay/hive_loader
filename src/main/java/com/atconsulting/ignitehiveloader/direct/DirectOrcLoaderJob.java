0a0d21bc2db6ebbb0d5037642d02646033ecb6c9
thatcoach <ppozerov@list.ru>
2016-08-30 21:09:47 +0300

    Switched to putAll for affinity mode.

--- src/main/java/com/atconsulting/ignitehiveloader/direct/DirectOrcLoaderJob.java
+++ src/main/java/com/atconsulting/ignitehiveloader/direct/DirectOrcLoaderJob.java
@@ -6,12 +6,16 @@ import org.apache.hadoop.hive.ql.io.orc.Reader;
 import org.apache.hadoop.hive.ql.io.orc.RecordReader;
 import org.apache.hadoop.hive.serde2.objectinspector.StructObjectInspector;
 import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCache;
 import org.apache.ignite.IgniteDataStreamer;
 import org.apache.ignite.IgniteException;
 import org.apache.ignite.cache.affinity.Affinity;
 import org.apache.ignite.cluster.ClusterNode;
 import org.apache.ignite.compute.ComputeJob;
 import org.apache.ignite.resources.IgniteInstanceResource;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Ignite job to load a file into Ignite.
@@ -54,6 +58,8 @@ public class DirectOrcLoaderJob implements ComputeJob {
         this.affMode = affMode;
     }
 
+
+
     /** {@inheritDoc} */
     @Override public Object execute() throws IgniteException {
         int rowCnt = 0;
@@ -62,9 +68,8 @@ public class DirectOrcLoaderJob implements ComputeJob {
 
         StructObjectInspector inspector = (StructObjectInspector)reader.getObjectInspector();
 
-        ClusterNode locNode = ignite.cluster().localNode();
-
-        Affinity<CHA.Key> aff = ignite.affinity(cacheName);
+        if (affMode)
+            return loadWithAffinityMode(reader, inspector);
 
         try (IgniteDataStreamer<CHA.Key, CHA> streamer = ignite.dataStreamer(cacheName)) {
             streamer.perNodeBufferSize(bufSize);
@@ -78,10 +83,6 @@ public class DirectOrcLoaderJob implements ComputeJob {
                     row = (OrcStruct)rows.next(row);
 
                     CHA.Key key = DirectOrcLoaderUtils.structToKey(row, inspector);
-
-                    if (affMode && !aff.isPrimary(locNode, key))
-                        continue;
-
                     CHA val = DirectOrcLoaderUtils.structToValue(row, inspector);
 
                     streamer.addData(key, val);
@@ -92,6 +93,60 @@ public class DirectOrcLoaderJob implements ComputeJob {
             catch (Exception e) {
                 throw new IgniteException("Failed to load ORC data to cache.", e);
             }
+        }
+
+        return rowCnt;
+    }
+
+    /**
+     * Perform load with affinity mode.
+     *
+     * @param reader Reader.
+     * @param inspector Inspector.
+     * @return Amount of loaded key-value pairs.
+     */
+    private int loadWithAffinityMode(Reader reader, StructObjectInspector inspector) {
+        IgniteCache<CHA.Key, CHA> cache = ignite.cache(cacheName);
+
+        int rowCnt = 0;
+
+        Map<CHA.Key, CHA> buf = new HashMap<>(bufSize, 1.0f);
+
+        ClusterNode locNode = ignite.cluster().localNode();
+
+        Affinity<CHA.Key> aff = ignite.affinity(cacheName);
+
+        try {
+            RecordReader rows = reader.rows();
+
+            OrcStruct row = null;
+
+            while (rows.hasNext()) {
+                row = (OrcStruct)rows.next(row);
+
+                CHA.Key key = DirectOrcLoaderUtils.structToKey(row, inspector);
+
+                if (affMode && !aff.isPrimary(locNode, key))
+                    continue;
+
+                CHA val = DirectOrcLoaderUtils.structToValue(row, inspector);
+
+                buf.put(key, val);
+
+                if (buf.size() == bufSize) {
+                    cache.putAll(buf);
+
+                    buf = new HashMap<>(bufSize, 1.0f);
+                }
+
+                rowCnt++;
+            }
+
+            if (buf.size() > 0)
+                cache.putAll(buf);
+        }
+        catch (Exception e) {
+            throw new IgniteException("Failed to load ORC data to cache.", e);
         }
 
         return rowCnt;
