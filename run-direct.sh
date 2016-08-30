42ac40593e6f44517ff79d58ca278c4c54a77ca8
Ivan Veselovskiy <iveselovskiy@gridgain.com>
2016-08-30 19:07:36 +0300

    added run script: wip.

---
+++ run-direct.sh
@@ -0,0 +1,21 @@
+HIVE_HOME=/opt/1964/apache-hive-1.2.1-bin
+IGNITE_HOME=/opt/1964/gridgain-professional-fabric-1.6.5
+
+#-----------------------------------------------
+# Unpacked Apache Hive binary distribution: 
+HIVE_HOME=${HIVE_HOME:-.../apache-hive-1.2.1-bin}
+
+# Unpacked Apache Hadoop binary distribution:
+HADOOP_HOME=${HADOOP_HOME:-.../hadoop-2.7.1}
+
+# Unpacked Ignite Fabric binary distribution:
+IGNITE_HOME=${IGNITE_HOME:-.../apache-ignite-fabric-1.7.0-SNAPSHOT-bin}
+
+
+# clean up output folder:
+#${HADOOP_HOME}/bin/hadoop fs -rm -r /tmp/ignite-hive-loader-out || true
+# Compose comma-separated list of libraries needed in the runtime: 
+#CP=${IGNITE_HOME}/libs/*:${IGNITE_HOME}/libs/ignite-spring/*:${IGNITE_HOME}/libs/optional/ignite-log4j/*:${HIVE_HOME}/lib/*
+CP='/mnt/nfsshare01/share/lib-share/*'
+
+java -cp target/ignite-hive-loader-1.0.0-SNAPSHOT.jar:${CP} com.atconsulting.ignitehiveloader.direct.DirectOrcLoaderRunner "${@}"
